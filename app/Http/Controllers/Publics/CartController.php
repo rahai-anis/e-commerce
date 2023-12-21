f40204626718241dd23b9e109be19db34ec3b388
Anis <a.rahai@inapi.org>
2023-12-21 14:45:57 +0100

    new template

---
+++ app/Http/Controllers/Publics/CartController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Controllers\Publics;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Cart;
+
+/*
+ * Used most of all for ajax requests
+ */
+
+class CartController extends Controller
+{
+
+    private $cart;
+
+    public function __construct()
+    {
+        $this->cart = new Cart();
+    }
+
+    public function addProduct(Request $request)
+    {
+        if (!$request->ajax()) {
+            abort(404);
+        }
+        $post = $request->all();
+        $quantity = (int) $post['quantity'];
+        if ($quantity == 0) {
+            $quantity = 1;
+        }
+        $this->cart->addProduct($post['id'], $quantity);
+    }
+
+    public function renderCartProductsWithHtml(Request $request)
+    {
+        if (!$request->ajax()) {
+            abort(404);
+        }
+        echo json_encode(array(
+            'html' => $this->cart->getCartHtmlWithProducts(),
+            'num_products' => $this->cart->countProducts
+        ));
+    }
+
+    public function removeProductQuantity(Request $request)
+    {
+        if (!$request->ajax()) {
+            abort(404);
+        }
+        $post = $request->all();
+        $this->cart->removeProductQuantity($post['id']);
+    }
+
+    public function getProductsForCheckoutPage(Request $request)
+    {
+        if (!$request->ajax()) {
+            abort(404);
+        }
+        echo $this->cart->getCartHtmlWithProductsForCheckoutPage();
+    }
+
+    public function removeProduct(Request $request)
+    {
+        if (!$request->ajax()) {
+            abort(404);
+        }
+        $post = $request->all();
+        $this->cart->removeProduct($post['id']);
+    }
+    public function download_pdf (Request $request){
+        if(Storage::disk("public")->exists($request -> file)){
+            $path = Storage::disk('public')->path($request -> file);
+            //dd($path);
+            $content = file_get_contents($path);
+            return response($content) -> withHeaders([
+                'Content-Type' => mime_content_type($path)
+            ]);
+        }
+        return "file not found !!";
+    }
+}
